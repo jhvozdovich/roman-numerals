3f42f79c502757515a3cce4247f72510fa72cb83
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-04-02 15:15:12 -0700

    add css styling and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,14 @@
 var romanNumbers = ["I", "V", "X", "L", "C", "D", "M"];
-var romanEquivilents = [1, 5, 10, 50, 100, 500, 1000];
+var romanEquivalents = [1, 5, 10, 50, 100, 500, 1000];
+
+
+
+
+$(document).ready(function() {
+  $("#user-input").submit(function(event) {
+    event.preventDefault();
+
+    
+    $(".result").show();
+  })
+})
